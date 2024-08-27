7c0e616d24020d00c32011362838d36d42555805
kenkomu <komukenneth216@gmail.com>
2024-08-27 13:38:18 +0300

    fermat little theorem

---
+++ fermats_little/target/debug/deps/fermats_little-f6380f5a264ee85a.d
@@ -0,0 +1,5 @@
+/home/komu/Maths_for_web3/Maths_for_web3/fermats_little/target/debug/deps/libfermats_little-f6380f5a264ee85a.rmeta: src/main.rs
+
+/home/komu/Maths_for_web3/Maths_for_web3/fermats_little/target/debug/deps/fermats_little-f6380f5a264ee85a.d: src/main.rs
+
+src/main.rs:
---
+++ fermats_little/target/debug/fermats_little.d
@@ -0,0 +1,1 @@
+/home/komu/Maths_for_web3/Maths_for_web3/fermats_little/target/debug/fermats_little: /home/komu/Maths_for_web3/Maths_for_web3/fermats_little/src/main.rs
