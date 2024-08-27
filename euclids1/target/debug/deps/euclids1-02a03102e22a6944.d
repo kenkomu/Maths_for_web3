d0715b522ba8defb020b45d8ce1c0de79e90441b
Kenneth Njoroge <95502725+kenkomu@users.noreply.github.com>
2024-08-27 11:09:24 +0300

    Delete euclids1/target directory

--- euclids1/target/debug/deps/euclids1-02a03102e22a6944.d
+++
@@ -1,5 +0,0 @@
-/home/komu/Maths_for_web3/Maths_for_web3/euclids1/target/debug/deps/libeuclids1-02a03102e22a6944.rmeta: src/main.rs
-
-/home/komu/Maths_for_web3/Maths_for_web3/euclids1/target/debug/deps/euclids1-02a03102e22a6944.d: src/main.rs
-
-src/main.rs:
